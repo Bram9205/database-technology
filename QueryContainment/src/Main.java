9c63c619f9902f7026830b1f5d35262db184d312
Jonathan Sumrall <jmsumrall@gmail.com>
2014-06-06 14:12:49 +0200

    example from sqlfiddle in jdbc

--- QueryContainment/src/Main.java
+++ QueryContainment/src/Main.java
@@ -2,9 +2,7 @@
 import data.Query;
 import tree.Tree;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
 	
@@ -32,8 +30,6 @@ public class Main {
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Cannot find the driver in the classpath!", e);
         }
-
-
         String url = "jdbc:mysql://localhost:3306/querydb";
         String username = "root";
         String password = "fletcher";
@@ -42,6 +38,95 @@ public class Main {
             System.out.println("Connecting database...");
             connection = DriverManager.getConnection(url, username, password);
             System.out.println("Database connected!");
+
+            Statement stmt = connection.createStatement();
+            String sql = "CREATE TABLE ab \n" +
+                    "\t(\n" +
+                    "     a varchar(20), \n" +
+                    "     b varchar(20)\n" +
+                    "     \n" +
+                    "    );\n" +
+                    "\n" +
+                    "CREATE TABLE bcd \n" +
+                    "\t(\n" +
+                    "     b varchar(20), \n" +
+                    "     c varchar(20),\n" +
+                    "     d varchar(20)\n" +
+                    "     \n" +
+                    "    );\n" +
+                    "CREATE TABLE bc\n" +
+                    "\t(\n" +
+                    "     b varchar(20), \n" +
+                    "     c varchar(20)\n" +
+                    "     \n" +
+                    "    );\n" +
+                    "CREATE TABLE db\n" +
+                    "\t(\n" +
+                    "     d varchar(20), \n" +
+                    "     b varchar(20)\n" +
+                    "     \n" +
+                    "    );\n" +
+                    "\n" +
+                    "\n" +
+                    "INSERT INTO ab\n" +
+                    "(a, b)\n" +
+                    "VALUES\n" +
+                    "('x', 'y');\n" +
+                    "\n" +
+                    "INSERT INTO bcd\n" +
+                    "(b, c, d)\n" +
+                    "VALUES\n" +
+                    "('y', 'y', 'z'),\n" +
+                    "('y', 'z', 'y'),\n" +
+                    "('y', 'z', 'z');\n" +
+                    "\n" +
+                    "\n" +
+                    "INSERT INTO bc\n" +
+                    "(b, c)\n" +
+                    "VALUES\n" +
+                    "('y', 'z');\n" +
+                    "\n" +
+                    "INSERT INTO db\n" +
+                    "(d, b)\n" +
+                    "VALUES\n" +
+                    "('z', 'y');\n" +
+                    "\n";
+
+            stmt.executeUpdate(sql);
+
+            sql = "select *\n" +
+                    "from ab\n" +
+                    "where exists(\n" +
+                    "    select * \n" +
+                    "    from bcd\n" +
+                    "    where exists(\n" +
+                    "      select *\n" +
+                    "      from db\n" +
+                    "      where bcd.d = db.d AND bcd.b = db.b\n" +
+                    "      ) and exists (\n" +
+                    "      select *\n" +
+                    "      from bc \n" +
+                    "      where bcd.b = bc.b AND bcd.c = bc.c\n" +
+                    "      ) and ab.b = bcd.b\n" +
+                    "  )\n" +
+                    "\n" +
+                    "\n";
+
+            stmt = connection.createStatement();
+            ResultSet RS = stmt.executeQuery(sql);
+            while(RS.next() != false)
+            {
+                System.out.println(RS.getString(1));
+            }
+
+            stmt.executeUpdate("DROP TABLE ab");
+            stmt.executeUpdate("DROP TABLE bcd");
+            stmt.executeUpdate("DROP TABLE bc");
+            stmt.executeUpdate("DROP TABLE db");
+
+
+
+
         } catch (SQLException e) {
             throw new RuntimeException("Cannot connect the database!", e);
         } finally {
