d0ac53ad3a0216a0e338671f5846ea632d8d4f27
Bram Kohl <bram4rjc@hotmail.com>
2014-06-06 13:57:23 +0200

    QueryGenerator is static
    Deleted instance of it and made static call

--- QueryContainment/src/Main.java
+++ QueryContainment/src/Main.java
@@ -8,15 +8,13 @@ import java.sql.SQLException;
 
 public class Main {
 	
-	private static QueryGenerator generator;
-	
 	private static void treeToSQL(Query query){
 		throw new UnsupportedOperationException("not yet implemented");
 	}
 	
 	
 	private static void test(){
-		Query query = generator.generateCyclicQueryWidth2(2500);
+		Query query = QueryGenerator.generateCyclicQueryWidth2(2500);
 		Tree tree = Tree.createFromCyclicQueryWidth2(query);
 		System.out.println(tree.toString());
 	}
