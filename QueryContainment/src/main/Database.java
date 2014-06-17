a965664eaf70abb2044687128d5048b06a284958
Jonathan Sumrall <jmsumrall@gmail.com>
2014-06-17 15:12:47 +0200

    adding method to get sum of records from each table, to verify claims about space requirement of the algorithm.

--- QueryContainment/src/main/Database.java
+++ QueryContainment/src/main/Database.java
@@ -95,7 +95,7 @@ public class Database {
         }
         ResultSet rs = this.queryForResultSet("SELECT COUNT(*) FROM " + node.getName());
         try {
-            System.out.println(rs.first());
+            while(rs.next()){System.out.println(rs.last());}
         }catch(Exception e){}
         return 0;
 
